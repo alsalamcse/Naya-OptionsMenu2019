8201be0cd4bea7bcd9cb0f19f8d96701bf887745
Owner <alsalamcse>
2019-09-29 08:55:07 +0300

    no78067

--- app/src/main/java/com/example/naya_optionsmenu2019/MainActivity.java
+++ app/src/main/java/com/example/naya_optionsmenu2019/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.naya_optionsmenu2019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +23,27 @@ public class MainActivity extends AppCompatActivity {
     {
         MenuInflater inflater=getMenuInflater();
         inflater.inflate(R.menu.my_menu,menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)//mo3alajat al7adath
+    {
+        if(item.getItemId()==R.id.itmScreen1)
+        {
+            Intent intent=new Intent(this,ActivityScreen1.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.itmScreen2)
+        {
+            Intent intent=new Intent(this,ActivityScreen2.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.itmYnet)
+        {
+            Toast.makeText(this,"ynet",Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
 //on mn alandroid
